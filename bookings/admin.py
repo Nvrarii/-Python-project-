b696d093e714452b5392eac56107f53c905dc137
Nurali Orazbekov <nrliorazbekov@gmail.com>
2025-04-09 23:41:03 +0500

    Изменения в admin.py

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Gym, Booking
 
 # Register your models here.
+
+# Регистрируем модели, чтобы они отображались в административной панели
+admin.site.register(Gym)
+admin.site.register(Booking)
