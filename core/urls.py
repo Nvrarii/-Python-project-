9988f013a9ffefd9a7d1a3591441c35df245a63f
Danial-iskakov <iskakovdanial7@gmail.com>
2025-04-20 16:35:46 +0600

    подключение навигационного меню

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
-# core/urls.py
-
 from django.urls import path
-from .views import home
+from . import views
+
+app_name = 'core'
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', views.home, name='home'),          # Главная страница
+    path('booking/', views.booking, name='booking'),  # Страница бронирования
+    path('about/', views.about, name='about'),      # Страница "О нас"
+    path('contact/', views.contact, name='contact'),  # Страница "Контакты"
 ]
